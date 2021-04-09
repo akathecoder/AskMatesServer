fc32838c143511c65855160ad30838c33a32ff7f
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-09 20:53:51 +0530

    Added Server Initialzation Code

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+var cors = require("cors");
+
+// Initialized Express App
+const app = express();
+
+// Added Cors Headers to Whitelist localhost:3000
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// parse requests of content-type - application/json
+// * https://medium.com/@mmajdanski/express-body-parser-and-why-may-not-need-it-335803cd048c
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to akathecoder application.",
+  });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
