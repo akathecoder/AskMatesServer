634c9a4870fc32666a970bb829af3bbb558a62cf
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-10 17:08:57 +0530

    Reverted back to mysql2

--- app/models/db.js
+++ app/models/db.js
@@ -1,22 +1,22 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const pool = await mysql.createPool({
-	host: dbConfig.HOST,
-	user: dbConfig.USER,
-	password: dbConfig.PASSWORD,
-	database: dbConfig.DB,
-	port: dbConfig.PORT,
-	waitForConnections: true,
-	connectionLimit: 3,
-	queueLimit: 0,
+const pool = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+  port: dbConfig.PORT,
+  waitForConnections: true,
+  connectionLimit: 3,
+  queueLimit: 0,
 });
 
 // open the MySQL connection
-pool.connect(error => {
-	if (error) throw error;
-	console.log("Successfully connected to the database.");
+pool.getConnection((err, conn) => {
+  if (err) throw err;
+  console.log("Successfully connected to the database.");
 });
 
 module.exports = pool;
