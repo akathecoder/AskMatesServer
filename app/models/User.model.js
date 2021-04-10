af1a98a203c7891fbc80482abe2b3dc84ad0f5c7
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-10 11:00:14 +0530

    Created a User Controller and changed User model to camelCase

--- app/models/User.model.js
+++ app/models/User.model.js
@@ -2,17 +2,19 @@ const sql = require("./db");
 
 // Constructor
 const User = (user) => {
-  this.Username = user.Username;
-  this.Password = user.Password;
-  this.First_Name = user.First_Name;
-  this.Middle_Name = user.Middle_Name || "";
-  this.Last_Name = user.Last_Name;
-  this.Email = user.Email;
-  this.Bio = user.Bio || "";
-  this.Batch = user.Batch;
-  this.Degree = user.Degree;
-  this.Field = user.Field;
-  this.Roll_No = user.Roll_No;
-  this.DOB = user.DOB;
-  this.Mobile_Number = user.Mobile_Number;
+  this.username = user.username;
+  this.password = user.password;
+  this.firstName = user.firstName;
+  this.middleName = user.middleName;
+  this.lastName = user.lastName;
+  this.email = user.email;
+  this.bio = user.bio;
+  this.batch = user.batch;
+  this.degree = user.degree;
+  this.field = user.field;
+  this.rollNo = user.rollNo;
+  this.dob = user.dob;
+  this.mobileNumber = user.mobileNumber;
 };
+
+modules.exports = User;
